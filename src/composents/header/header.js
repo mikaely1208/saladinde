f7f47e9ddff74109985eeed0074987de570d5738
andrianandraina.mika <mikaely.andrianandraina@esqese.ucly.fr>
2022-12-07 09:03:21 +0100

    debut header

---
+++ src/composents/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../assets/bol.svg';
+import { ReactComponent as Home } from  '../../assets/house.svg';
+import { ReactComponent as Explore } from '../../assets/explore.svg';
+
+const Header = () => {
+    return(
+        <nav>
+            <div></div>
+        </nav>
+    )
+}
